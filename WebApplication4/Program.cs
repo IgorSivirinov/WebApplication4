e286a84d05c52a4a302efc58fa292da95e0bcd88
Игорь Сивиринов <igor.sivirinov.vasilevich@gmail.com>
2021-11-12 19:50:48 +0200

    Добавьте файлы проекта.

---
+++ WebApplication4/Program.cs
@@ -0,0 +1,62 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//if (app.Environment.IsDevelopment())
+//{
+
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+       new WeatherForecast
+       (
+           DateTime.Now.AddDays(index),
+           Random.Shared.Next(-20, 55),
+           summaries[Random.Shared.Next(summaries.Length)]
+       ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+
+app.MapPost("/hello", (User user) =>
+{
+    return new HelloResponse{ Body = $"Hello {user.Name}({user.Id})" };
+});
+
+app.Run();
+
+
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public class User
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class HelloResponse
+{
+    public string Body { get; set; }
+}
